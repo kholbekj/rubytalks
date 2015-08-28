2088b339e8942b489b8920d58b8bf2adb52f7190
Kasper Holbek Jensen <khj@autobutler.dk>
2015-08-28 20:03:52 +0200

    go to sane bootstrap version, and add youtube embeds

--- lib/rubytalks/entities/talk.rb
+++ lib/rubytalks/entities/talk.rb
@@ -1,4 +1,22 @@
 class Talk
   include Lotus::Entity
   attributes :title, :author, :link
+
+  def embed
+    return "" unless embeddable?
+    "<iframe width=\"100%\" height=\"auto\" src=\"#{embeddable_link}\" frameborder=\"0\" allowfullscreen></iframe>"
+  end
+
+  private
+  def embeddable_link
+    @embeddable_link ||= begin
+      parts = /(.+?youtube\.com\/)watch\?v=(.+?)(&|$)/.match(link)
+      return nil unless parts && parts.length > 1
+      "#{parts[1]}embed/#{parts[2]}"
+    end
+  end
+
+  def embeddable?
+    !embeddable_link.nil?
+  end
 end
